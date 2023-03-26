284f596dca5877b28758dbd2978851d0a78e61d1
boring-km <kms0644804@naver.com>
2023-03-26 22:30:46 +0900

    kangmin: leetcode 105번 문제 좀 더 단순하게

--- kangmin/src/study/ConstructBinaryTreeFromPreAndInOrderTraversal.kt
+++ kangmin/src/study/ConstructBinaryTreeFromPreAndInOrderTraversal.kt
@@ -2,6 +2,7 @@ package study
 
 // https://luckydavekim.github.io/algorithm/leetcode/construct-binary-tree-from-preorder-and-inorder-traversal
 // Python 예제에서는 Array를 slice해서 구현하는데 Kotlin에서는 런타임에러가 자꾸 나온다.
+// 성공: https://velog.io/@wlgns2223/leetcode-105-Construct-Binary-Tree-From-Preorder-and-Inorder-Traversal
 object ConstructBinaryTreeFromPreAndInOrderTraversal {
     class TreeNode(var `val`: Int) {
         var left: TreeNode? = null
@@ -9,44 +10,19 @@ object ConstructBinaryTreeFromPreAndInOrderTraversal {
     }
 
     fun buildTree(preorder: IntArray, inorder: IntArray): TreeNode? {
-        return recursive(preorder, inorder, 0, 0, inorder.size - 1)
+        return recursive(preorder.toMutableList(), inorder.toMutableList())
     }
 
-    private fun recursive(
-        preorder: IntArray,
-        inorder: IntArray,
-        preIndex: Int,
-        inStartIndex: Int,
-        inEndIndex: Int
-    ): TreeNode? {
-        if (preIndex >= preorder.size || inStartIndex > inEndIndex) {
+    private fun recursive(preorderList: MutableList<Int>, inorder: List<Int>): TreeNode? {
+        if (inorder.isNotEmpty()) {
+            val index = inorder.indexOf(preorderList.removeAt(0))
+            val root = TreeNode(inorder[index])
+            root.left = recursive(preorderList, inorder.slice(IntRange(0, index-1)))
+            root.right = recursive(preorderList, inorder.slice(IntRange(index+1, inorder.size-1)))
+            return root
+        } else {
             return null
         }
-        val node = TreeNode(preorder[preIndex])
-        var inorderIndex = 0
-        for (i in inStartIndex..inEndIndex) {
-            if (inorder[i] == node.`val`) {
-                inorderIndex = i
-                break
-            }
-        }
-
-        inorder.indexOf(node.`val`)
-
-        node.left = recursive(
-            preorder,
-            inorder,
-            preIndex + 1,
-            inStartIndex,
-            inorderIndex - 1
-        )
-        node.right = recursive(
-            preorder,
-            inorder,
-            preIndex + inorderIndex - inStartIndex + 1,
-            inorderIndex + 1, inEndIndex
-        )
-        return node
     }
 }
 
@@ -55,7 +31,7 @@ fun main() {
     val preorder = intArrayOf(3, 9, 20, 15, 7)
     val inorder = intArrayOf(9, 3, 15, 20, 7)
 
-//    val result = inorder.slice(IntRange(3, inorder.size-1))
+//    val result = inorder.slice(IntRange(2, 4))
 //    result.forEach { print("$it ") }
 
     ConstructBinaryTreeFromPreAndInOrderTraversal.buildTree(preorder, inorder)
