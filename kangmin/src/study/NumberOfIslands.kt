291245bcae5ed6d5e1320df77ce98627fbc44df1
boring-km <kms0644804@naver.com>
2023-04-13 00:44:13 +0900

    kangmin: leetcode 200번 풀이 성공 - 자료형 LinkedList 사용하면 됨

--- kangmin/src/study/NumberOfIslands.kt
+++ kangmin/src/study/NumberOfIslands.kt
@@ -1,20 +1,15 @@
 package study
 
-import java.util.Deque
-import java.util.LinkedList
-import java.util.Queue
+import java.util.*
 
 object NumberOfIslands {
     fun numIslands(grid: Array<CharArray>): Int {
-
         val n = grid.size
         val m = grid[0].size
 
         var answer = 0
 
         val visited = Array(n) { BooleanArray(m) }
-
-
 
         for (y in 0 until n) {
             for (x in 0 until m) {
@@ -35,14 +30,14 @@ object NumberOfIslands {
         m: Int,
         grid: Array<CharArray>
     ) {
-        val queue: Queue<IntArray> = LinkedList()
-        queue.add(intArrayOf(y, x))
+        val queue = LinkedList<IntArray>()
+        queue.addFirst(intArrayOf(y, x))
         val move = arrayOf(
             intArrayOf(1, 0), intArrayOf(0, 1), intArrayOf(-1, 0), intArrayOf(0, -1)
         )
 
         while (queue.isNotEmpty()) {
-            val temp = queue.poll()
+            val temp = queue.removeFirst()
             val ty = temp[0]
             val tx = temp[1]
             visited[ty][tx] = true
@@ -52,11 +47,12 @@ object NumberOfIslands {
                 val mx = tx + move[i][1]
 
                 if (my in 0 until n && mx in 0 until m && grid[my][mx] == '1' && !visited[my][mx]) {
-                    queue.add(intArrayOf(my, mx))
+                    queue.addFirst(intArrayOf(my, mx))
                 }
             }
         }
     }
+
 }
 
 fun main() {
